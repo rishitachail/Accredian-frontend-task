9607f8b8ed845e1ec20c8787e4acd999d612aad7
Rishita Chail <108628119+rishitachail@users.noreply.github.com>
2023-12-09 23:54:04 +0530

    Add files via upload

---
+++ Accredian-frontend-task/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import LoginPage from './components/Login/Login';
+
+
+function App() {
+  return (
+    <div>
+      <LoginPage/>
+      
+    </div>
+  );
+}
+
+export default App;
